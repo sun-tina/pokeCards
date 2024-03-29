6050bc12cef2de8db9beb28ccd722ae29db622a8
Tina Sun <suntina@google.com>
2024-03-29 17:06:09 -0400

    poke title

--- pokeCards/src/App.jsx
+++ pokeCards/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import Cards from './components/Cards'
-import './App.css'
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import NavBar from "./components/layout/NavBar"
 
-function App() {
-  const [card, setCard] = useState()
-  return (
-    <>
-    </>
-  )
+class App extends Component{
+  render(){
+    return <div className="App">
+      <h1>good job, keep it up! keep going. you got this.</h1>
+      <NavBar/>
+    </div>
+  }
 }
-
 
 export default App
