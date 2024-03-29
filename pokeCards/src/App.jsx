28dea3f02576ee711036a8a13df0e01c55517abc
Tina Sun <suntina@google.com>
2024-03-29 17:31:51 -0400

    card grid set up

--- pokeCards/src/App.jsx
+++ pokeCards/src/App.jsx
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import NavBar from "./components/layout/NavBar"
+import Dashboard from "./components/layout/Dashboard";
 
 class App extends Component{
   render(){
-    return <div className="App">
-      <h1>good job, keep it up! keep going. you got this.</h1>
-      <NavBar/>
+    return (
+      <div className="App">
+        <NavBar/>
+        <div className="container">
+          <Dashboard/>
+       </div>
     </div>
+    );
   }
 }
 
